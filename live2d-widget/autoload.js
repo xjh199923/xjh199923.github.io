bc247a0cb4f2309691091d9e1b1f1c8d95c086be
xjh199923 <981715157@qq.com>
2020-04-15 17:14:03 +0800

    Site updated: 2020-04-15 17:14:01

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,3 +1,3 @@
-// build time:Wed Apr 15 2020 12:45:13 GMT+0800 (中国标准时间)
+// build time:Wed Apr 15 2020 17:13:48 GMT+0800 (中国标准时间)
 const live2d_path="https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/";$("<link>").attr({href:live2d_path+"waifu.css",rel:"stylesheet"}).appendTo("head");$.ajax({url:live2d_path+"live2d.min.js",dataType:"script",cache:true});$.ajax({url:live2d_path+"waifu-tips.js",dataType:"script",cache:true});$(window).on("load",function(){initWidget(live2d_path+"waifu-tips.json","https://live2d.fghrsh.net/api")});
 //rebuild by neat 
