89e0f1af9eb5ba3949f561abfdb16976be5d1723
xjh199923 <981715157@qq.com>
2020-04-16 13:14:42 +0800

    Site updated: 2020-04-16 13:14:40

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Apr 16 2020 13:02:23 GMT+0800 (中国标准时间)
+// build time:Thu Apr 16 2020 13:14:29 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
