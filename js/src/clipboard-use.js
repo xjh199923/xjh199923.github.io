7b7788712f4f6d43d2b9cefdeb5ccfcd3a0c2e12
xjh199923 <981715157@qq.com>
2020-09-17 10:24:58 +0800

    Site updated: 2020-09-17 10:24:58

--- js/src/clipboard-use.js
+++
@@ -1,3 +0,0 @@
-// build time:Thu Jul 30 2020 14:22:06 GMT+0800 (中国标准时间)
-!function(n,t,a){var i=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>复制</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};i()}(window,document);
-//rebuild by neat 
